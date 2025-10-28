83bb237d175e784cd5259a1756bd1916d4f55447
lupus117 <stenunt117@gmail.com>
2025-10-28 18:29:47 +0200

    Update uimanager.cpp

--- src/uimanager.cpp
+++ src/uimanager.cpp
@@ -7,6 +7,10 @@
 #include "reportmanager.h"
 #include "materialmanager.h"
 
+#include <functional>
+
+//	UImanager* uimanager = new UImanager(order_manager, client_manager, employee_manager, photoreport_manager, receptreport_manager, material_manager);
+
 class UImanager
 {
 private:
@@ -18,7 +22,7 @@ private:
     ClientManager *client_manager;
 
 public:
-    PhotoStudioManager(*OrderManager omanager, *ClientManager climanager, *EmployeeManager empmanager, *PhotoReportManager prepmanager, *ReceptReportManager repmanager, *MaterialManager matmanager)
+    UImanager(OrderManager *omanager, ClientManager *climanager, EmployeeManager *empmanager, PhotoReportManager *prepmanager, ReceptReportManager *repmanager, MaterialManager *matmanager)
     {
         order_manager = omanager;
         receptreport_manager = repmanager;
@@ -28,23 +32,52 @@ public:
         client_manager = climanager;
     }
 
-    void view_list_users(){
-        map<int, std::shared_ptr<Employee>> *employees = employee_manager->getEmployyees(); 
+    void view_list_users()
+    {
+        map<int, std::shared_ptr<Employee>> employees = employee_manager->getEmployees();
 
-        for (auto const& [key, val] : employees){
-            std::cout >> "Employee ID: " >> key >> endl; 
+        for (auto const &[key, val] : employees)
+        {
+            std::cout << "Employee ID: " << key << " Employee Name: " << val->emp_name << endl;
         }
+        return;
     }
 
-    int view_main(){
-        parser = cmdParser<int>("Photography Studio main view")
-        parser.addCommand("List Users", [](){
+    int view_main()
+    {
+        cmdParser<int> parser("Photography Studio main view");
+        // function lusers = [this](){view_list_users();};
+        int currentuser = 0;
+        parser.addCommand("List Users", [this]()
+                          {
             view_list_users();
-            return 1;
-        })
-
+            return 1; });
+        parser.addCommand("Login", [this, currentuser]()
+                          {
+            std::cout << "to be implemented";
+            return 1; });
+        parser.addCommand("List Orders", [this]()
+                          {
+            std::cout << "to be implemented";
+            return 1; });
+        //  parser.listCommands();
+        parser.loopCommands();
+        return 1;
     }
 };
+/*
+int main(){
+    OrderManager* order_manager = new OrderManager();
+    ReceptReportManager* receptreport_manager = new ReceptReportManager();
+    PhotoReportManager* photoreport_manager = new PhotoReportManager();
+    EmployeeManager* employee_manager = new EmployeeManager();
+    MaterialManager* material_manager = new MaterialManager();
+    ClientManager* client_manager = new ClientManager();
+    UImanager* uimanager = new UImanager(order_manager, client_manager, employee_manager, photoreport_manager, receptreport_manager, material_manager);
+    uimanager->view_main();
+}
+*/
+
 /*
 Views tree:
 Main
@@ -73,8 +106,8 @@ ManageOrder
                     ChangeOrderStatus
                     MarkMaterials
                     SubmitReport
-                    
-                    
+
+
                     Exit
-                    
+
 */
