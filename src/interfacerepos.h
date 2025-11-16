83da24d8250ae21a8aac1e2a287d5b4c10b10003
doc <allanjrw@ciffran.com>
2025-11-16 23:36:10 +0200

    Order.cpp & order.h removal of const & enum definitions

---
+++ src/interfacerepos.h
@@ -0,0 +1,22 @@
+#ifndef INTERFACEREPOS_H
+#define INTERFACEREPOS_H
+
+class IMaterialManager {
+
+};
+
+class IEmployeeManager {
+
+};
+
+class IClientManager {
+
+};
+
+class IReportManager {
+
+};
+
+//for each, make logic classes like Employees depend on these instead of their non abstract brethren
+
+#endif
