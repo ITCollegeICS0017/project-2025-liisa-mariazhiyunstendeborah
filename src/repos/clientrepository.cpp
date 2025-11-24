c352a1000e150179e2800d3b91c54c688b8f0192
doc <allanjrw@ciffran.com>
2025-11-24 18:14:18 +0200

    formatting

--- src/repos/clientrepository.cpp
+++ src/repos/clientrepository.cpp
@@ -1,41 +1,45 @@
 #include "clientrepository.h"
 
-const std::map<int, std::shared_ptr<Client>>& ClientRepository::getClients() const {
-            return clients;
+const std::map<int, std::shared_ptr<Client>>& ClientRepository::getClients()
+    const {
+  return clients;
 }
 
 Client* ClientRepository::findClient(int client_id) {
-    auto iter = clients.find(client_id);
-    if (iter != clients.end()) {
-        return iter->second.get();
-    }
-    return nullptr;
+  auto iter = clients.find(client_id);
+  if (iter != clients.end()) {
+    return iter->second.get();
+  }
+  return nullptr;
 }
 
-//checks if client's id is default, if so it sets the id to the current next id (which is then incremented) and adds the client to the repository
-//if client's id is not default, assumes client has already been added to repository and throws error
+// checks if client's id is default, if so it sets the id to the current next id
+// (which is then incremented) and adds the client to the repository if client's
+// id is not default, assumes client has already been added to repository and
+// throws error
 int ClientRepository::addClient(std::shared_ptr<Client> client) {
-    if (client->client_id == 0) {
+  if (client->client_id == 0) {
     int client_id = next_id++;
     client->client_id = client_id;
     clients.insert({client_id, client});
     return client_id;
-    } else {
-        throw std::invalid_argument("Client already exists!");
-    }
+  } else {
+    throw std::invalid_argument("Client already exists!");
+  }
 }
 
-void ClientRepository::editClient(int client_id, std::shared_ptr<Client> updated_client) {
-    if (!findClient(client_id)) {
-        throw std::invalid_argument("Client not found!");
-    }
-    clients.at(client_id) = updated_client;
+void ClientRepository::editClient(int client_id,
+                                  std::shared_ptr<Client> updated_client) {
+  if (!findClient(client_id)) {
+    throw std::invalid_argument("Client not found!");
+  }
+  clients.at(client_id) = updated_client;
 }
 
 void ClientRepository::deleteClient(int client_id) {
-    if (!findClient(client_id)) {
-        throw std::invalid_argument("Client does not exist to be deleted!");
-    } else {
-        clients.erase(client_id);
-    }
+  if (!findClient(client_id)) {
+    throw std::invalid_argument("Client does not exist to be deleted!");
+  } else {
+    clients.erase(client_id);
+  }
 }
--- src/repos/orderrepository.cpp
+++ src/repos/orderrepository.cpp
@@ -1,63 +1,69 @@
 #include "orderrepository.h"
 
-//Upon not finding an order, returns a nullptr rather than return an error, so it's easier to use in other functions for checking if an order exists.
+// Upon not finding an order, returns a nullptr rather than return an error, so
+// it's easier to use in other functions for checking if an order exists.
 Order* OrderRepository::findOrder(int orderid) {
-    auto iter = orders.find(orderid);
-    if (iter != orders.end()) {
-        return iter->second.get();
+  auto iter = orders.find(orderid);
+  if (iter != orders.end()) {
+    return iter->second.get();
+  }
+  return nullptr;
+}
+
+const std::map<int, std::shared_ptr<Order>>& OrderRepository::getOrders()
+    const {
+  return orders;
+}
+
+// returns map of orders with the completed CompletionStatus in the repository,
+// does not but realistically will need to only find orders of a day/week
+std::map<int, std::shared_ptr<Order>> OrderRepository::getCompletedOrders()
+    const {
+  std::map<int, std::shared_ptr<Order>> completed_orders;
+  for (const auto& order : orders) {
+    if (order.second->compl_status == CompletionStatus::Completed) {
+      completed_orders.insert(order);
     }
-    return nullptr;
+  }
+  return completed_orders;
 }
 
-const std::map<int, std::shared_ptr<Order>>& OrderRepository::getOrders()  const  {
-    return orders;
+float OrderRepository::calculateProfits(
+    const std::map<int, std::shared_ptr<Order>>& completed_orders) const {
+  float profits = 0.0f;
+  for (const auto& order : completed_orders) {
+    profits += order.second->price;
+  }
+  return profits;
 }
 
-//returns map of orders with the completed CompletionStatus in the repository, 
-//does not but realistically will need to only find orders of a day/week
-std::map<int, std::shared_ptr<Order>> OrderRepository::getCompletedOrders()  const {
-    std::map<int, std::shared_ptr<Order>> completed_orders;
-    for (const auto& order : orders) {
-        if (order.second->compl_status == CompletionStatus::Completed) {
-            completed_orders.insert(order);
-        }
-    }
-    return completed_orders;
-}
-
-float OrderRepository::calculateProfits(const std::map<int, std::shared_ptr<Order>>& completed_orders) const {
-    float profits = 0.0f;
-        for (const auto& order : completed_orders) {
-            profits += order.second->price;
-        }
-    return profits;
-}
-
-//checks that input order still has a default orderid, if so orderid is set to next_id (which is then incremented) and order is added to repository
-//if the above is not true, throws an error
-int OrderRepository::addOrder(std::shared_ptr<Order> order)  {
-    if (order->orderid == 0) {
+// checks that input order still has a default orderid, if so orderid is set to
+// next_id (which is then incremented) and order is added to repository if the
+// above is not true, throws an error
+int OrderRepository::addOrder(std::shared_ptr<Order> order) {
+  if (order->orderid == 0) {
     int orderid = next_id++;
     order->orderid = orderid;
     orders.insert({orderid, order});
     return orderid;
-    } else {
-        throw std::invalid_argument("Order already exists!");
-    }
+  } else {
+    throw std::invalid_argument("Order already exists!");
+  }
 }
 
-void OrderRepository::editOrder(int orderid, std::shared_ptr<Order> updated_order)  {
-    if (!findOrder(orderid)) {
-        throw std::invalid_argument("Order not found!");
-    } else {
-        orders.at(orderid) = updated_order;
-    }
+void OrderRepository::editOrder(int orderid,
+                                std::shared_ptr<Order> updated_order) {
+  if (!findOrder(orderid)) {
+    throw std::invalid_argument("Order not found!");
+  } else {
+    orders.at(orderid) = updated_order;
+  }
 }
 
-void OrderRepository::deleteOrder(int orderid)  {
-    if (!findOrder(orderid)) {
-        throw std::invalid_argument("Order does not exist to be deleted!");
-    } else {
-        orders.erase(orderid);
-    }
+void OrderRepository::deleteOrder(int orderid) {
+  if (!findOrder(orderid)) {
+    throw std::invalid_argument("Order does not exist to be deleted!");
+  } else {
+    orders.erase(orderid);
+  }
 }
